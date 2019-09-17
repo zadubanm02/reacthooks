6be49f2f7c0415d8e78a8a106d01f0796a54eb17
Michal Zaduban <zadubanm@gmail.com>
2019-09-17 16:16:59 +0200

    Jumbo earth pic

--- src/components/jumbo/jumbo.jsx
+++ src/components/jumbo/jumbo.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { Jumbotron } from "react-bootstrap";
 import "./jumbo-styles.css";
+import pic from "../../earthpic.png";
+import "./pic-styles.css";
 
 const Jumbo = () => {
   return (
     <div>
       <Jumbotron className="jumbo">
         <h1>Hello, world!</h1>
-        <p>
-          This is a simple hero unit, a simple jumbotron-style component for
-          calling extra attention to featured content or information.
-        </p>
+        <p>This is a simple hero unit</p>
+        <img src={pic} alt="earth" className="pic" />
       </Jumbotron>
     </div>
   );
